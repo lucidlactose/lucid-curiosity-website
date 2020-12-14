ab12039f82cd30ff067b3fa90b401feb6607278d
lucidlactose <jesusvillarruel98@gmail.com>
2020-12-13 22:22:17 -0800

    pulling stuff out of the header and footer to normalize the webpage format across the site

--- src/shared-header/shared-header-thing.js
+++ src/shared-header/shared-header-thing.js
@@ -2,29 +2,42 @@
 import React from 'react';
 import {styled} from 'fusion-plugin-styletron-react';
 import {HeaderBackgroundColor} from '../styles';
-import {assetUrl} from 'fusion-core';
 
-const Wrapper = styled('div', {
-  backgroundColor: HeaderBackgroundColor,
-  padding: '10px',
-  margin: '-8px -8px 0px -8px',
+const BackgroundWrapper = styled('div', {
+  background: HeaderBackgroundColor,
+  position: 'fixed',
+  height: '50px',
+  top: '0',
+  left: '0',
+  right: '0',
+  zIndex: '999',
 });
 
-const ImageWrapper = styled('img', {
-  margin: '8px 0px 0px 16px',
-  width: '40px',
-  border: '1px solid black',
+const AlignmentWrapper = styled('div', {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  lineHeight: '2',
+  margin: '0 auto',
+  maxWidth: '1420px',
 });
 
-const ProfileWrapper = styled('div', {
+const NameWrapper = styled( 'div', {
+  fontSize: '24px',
+});
+
+const ProfileWrapper = styled( 'div', {
   color: 'green',
 });
 
+
 const SharedHeaderThing = () => (
-  <Wrapper>
-    <ImageWrapper src={assetUrl('../assets/jolly_roger.png')} />
-    <ProfileWrapper />
-  </Wrapper>
+  <BackgroundWrapper>
+    <AlignmentWrapper>
+      <NameWrapper> (Img maybe?) SITE NAME </NameWrapper>
+      <ProfileWrapper>Home</ProfileWrapper>
+    </AlignmentWrapper>
+  </BackgroundWrapper>
 );
 
 export default SharedHeaderThing;
---
+++ src/values.js
@@ -0,0 +1,2 @@
+// values
+export const MaxPageLength = '1420px';
