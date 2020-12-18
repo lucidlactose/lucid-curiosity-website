02189e8ff0a6acc891f2e7c1b03484caac691a7f
lucidlactose <jesusvillarruel98@gmail.com>
2020-12-17 23:03:03 -0800

    The footer is now presentable. Expect CSS update next.

--- src/home-page/home-page.js
+++ src/home-page/home-page.js
@@ -9,13 +9,13 @@ const Center = styled('div', {
   alignItems: 'center',
   justifyContent: 'center',
   height: '100%',
-  marginTop: '64px',
-  padding: '64px',
 });
 
 const FullHeightDiv = styled('div', {
   height: '100%',
   backgroundColor: '#eeeeee',
+  maxWidth: '1420px',
+  margin: '0 auto',
 });
 
 const HomePage = () => (
--- src/shared-footer/shared-footer-thing.js
+++ src/shared-footer/shared-footer-thing.js
@@ -4,41 +4,38 @@ import {styled} from 'fusion-plugin-styletron-react';
 import {HeaderBackgroundColor} from '../styles';
 import {assetUrl} from 'fusion-core';
 
-/*
- this wrapper will result in a grid that is something like
-         |-----------------|-----------------|-----------------|
-         |                 |                 |                 |
-         |                 |                 |                 |
-         |        A        |        B        |        C        |
-         |                 |                 |                 |
-         |                 |                 |                 |
-         |-----------------|-----------------|-----------------|
-  Where those that do not fit in A will go to B and so on.
-  If one fits in another is dependent on screen size or whatnot, so account for that later
- */
-
 const Wrapper = styled('div', {
-  color: 'blue',
   backgroundColor: HeaderBackgroundColor,
   margin: '-8px',
-  columnCount: '3',
+});
+
+const AlignmentWrapper = styled('div', {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  lineHeight: '2',
+  margin: '0 auto',
+  maxWidth: '1420px',
+  minHeight: '256px',
+  margin: '0 auto',
 });
 
 const ImageWrapper = styled('img', {
-  padding: '8vh 16vw',
   width: '100px',
+  height: '100%',
 });
 
 const TextWrapper = styled('div', {
-  padding: '8vh',
   color: 'black',
 });
 
 const SharedFooterThing = () => (
   <Wrapper>
-    <ImageWrapper src={assetUrl('../assets/jolly_roger.png')} />
-    <TextWrapper>Something</TextWrapper>
-    <TextWrapper>Something</TextWrapper>
+    <AlignmentWrapper>
+      <ImageWrapper src={assetUrl('../assets/jolly_roger.png')} />
+      <TextWrapper>Something</TextWrapper>
+      <TextWrapper>Something</TextWrapper>
+    </AlignmentWrapper>
   </Wrapper>
 );
 
